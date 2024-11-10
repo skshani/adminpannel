b258cfa77a05b8fd8183bcd825fb3640287b10b8
skshani <skshani4930@gmail.com>
2024-11-10 17:24:24 +0530

    admin pannel

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+import Header from './Header'
+import Home from './Home'
+import Sidebar from './Sidebar'
+
+
+function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar} />
+
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar} />
+      <Home />
+    </div>
+  )
+}
+
+export default App
